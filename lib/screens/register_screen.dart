ab1ff472839ec06f1451e4f84df4c4081986d8f5
Dipika <maharjandipika49@gmail.com>
2025-12-05 14:40:35 +0545

    register screen

---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,208 @@
+import 'package:flutter/material.dart';
+import 'package:trip_wise_nepal/screens/login_screen.dart';
+import 'package:trip_wise_nepal/widgets/my_button.dart';
+import 'package:trip_wise_nepal/widgets/my_textfield.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 40),
+
+                // Logo
+                Center(
+                  child: Image.asset(
+                    'assets/images/logo2.png',
+                    height: 120,
+                    width: 120,
+                  ),
+                ),
+                const SizedBox(height: 15),
+
+                // Title
+                Center(
+                  child: Text(
+                    "Create new account",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.teal,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+
+                // Email field
+                MyTextfield(
+                  controller: emailController,
+                  hintText: "Enter your email address",
+                  labelText: "Email Address",
+                  errorMessage: "Please enter your email",
+                ),
+                const SizedBox(height: 25),
+
+
+                // Password field
+                MyTextfield(
+                  controller: passwordController,
+                  hintText: "Enter your password",
+                  labelText: "Password",
+                  errorMessage: "Please enter your password",
+                ),
+                const SizedBox(height: 25),
+
+                // Confrm Password field
+                MyTextfield(
+                  controller: confirmPasswordController,
+                  hintText: "Enter your password",
+                  labelText: "Confirm Password",
+                  errorMessage: "Please enter your password",
+                ),
+                const SizedBox(height: 10),
+
+
+                // Register button
+                Center(
+                  child: MyButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginScreen()),
+                        );
+                      },
+                      text: "Register"),
+                ),
+
+                const SizedBox(height: 25),
+
+                Row(
+                  children: [
+                    Expanded(child: Divider(thickness: 1)),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8),
+                      child: Text(
+                        "Or continue with",
+                        style: TextStyle(fontSize: 14, color: Colors.black54),
+                      ),
+                    ),
+                    Expanded(child: Divider(thickness: 1)),
+                  ],
+                ),
+
+                const SizedBox(height: 25),
+
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    height: 48,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black54),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset("assets/images/google.png", height: 22), // add icon
+                        const SizedBox(width: 10),
+                        Text(
+                          "Continue with Google",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 12),
+
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    height: 48,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black54),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset("assets/images/facebook.png", height: 22), // add icon
+                        const SizedBox(width: 10),
+                        Text(
+                          "Continue with Facebook",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 25),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Already have an account? ",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const LoginScreen()),
+                        );
+                      },
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.teal,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
