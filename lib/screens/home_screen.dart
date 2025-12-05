a97dacf74ba0dfbc44a4206cbf339975bd9242e6
Dipika <maharjandipika49@gmail.com>
2025-12-05 14:43:15 +0545

    home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(80.0),
+        child: Text(
+            "This is home screen",
+        ),
+      ),
+    );
+  }
+}
